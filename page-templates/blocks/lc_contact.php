29aab771ab1137e3a458dd5d694dc69a73a4ceab
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-08-25 19:27:38 +0100

    Update lc_contact.php

--- page-templates/blocks/lc_contact.php
+++ page-templates/blocks/lc_contact.php
@@ -1,11 +1,11 @@
 <section class="contact">
     <div class="container-xl py-5">
         <div class="row">
-            <div class="col-md-6 offset-md-1">
+            <div class="col-md-6 offset-lg-1">
                 <h2>Say Hello</h2>
                 <?=do_shortcode('[contact-form-7 id="' . get_field('form_id') . '"]')?>
             </div>
-            <div class="col-md-3 offset-md-1">
+            <div class="col-md-3 offset-lg-1">
                 <h2>Booking</h2>
                 <?=get_field('booking')?>
             </div>
