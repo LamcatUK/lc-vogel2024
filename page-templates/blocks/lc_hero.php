828c01505923b5521bc4fd626f8f0851938da987
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-08-25 19:25:57 +0100

    Update lc_hero.php

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -1,7 +1,7 @@
 <?php
 $title = get_field('title') ?: get_the_title();
 ?>
-<section class="hero">
+<section class="hero pt-5">
     <div class="container-xl py-5">
         <h1 class="text-center"><?=$title?></h1>
     </div>
